856b2286ef101358a5d36581f87562fa53be7522
Adrian Conza <hola@adrianconza.com>
2017-03-22 00:13:55 -0500

    changes crud

--- controllers/cooperativa.js
+++ controllers/cooperativa.js
@@ -6,65 +6,49 @@ function getCooperativa (req, res) {
   let cooperativaId = req.params.cooperativaId
   Cooperativa.findById(cooperativaId)
   .then(cooperativa => {
-      if(!cooperativa) return res.status(404).send({ message: 'La cooperativa no existe' })
-      res.status(200).send({ cooperativa })
+    if(!cooperativa) return res.status(404).send({ message: `La cooperativa ${cooperativaId} no existe` })
+    res.status(200).send({ cooperativa })
   })
-  .catch(err => res.status(500).send({ message: `Error al realizar la peticion: ${err}` }))
+  .catch(err => res.status(500).send({ message: `Error al realizar la consulta: ${err}` }))
 }
 
 function getCooperativas (req, res) {
   Cooperativa.findAll()
   .then(cooperativas => {
-      if(!cooperativas) return res.status(404).send({ message: 'No existen cooperativas' })
-      res.status(200).send({ cooperativas })
+    if(cooperativas.length <= 0) return res.status(404).send({ message: 'No existen cooperativas' })
+    res.status(200).send({ cooperativas })
   })
-  .catch(err => res.status(500).send({ message: `Error al realizar la peticion: ${err}` }))
+  .catch(err => res.status(500).send({ message: `Error al realizar la consulta: ${err}` }))
 }
 
 function saveCooperativa (req, res) {
-  console.log(req.body)
-
-  let cooperativa = new Cooperativa()
-  cooperativa.codigo = req.body.codigo
-
+  let cooperativa = req.body
   Cooperativa.create(cooperativa)
-  .then((cooperativaStored) => {
-      res.status(200).send({ cooperativa: cooperativaStored })
+  .then(cooperativaStored => {
+    res.status(200).send({ cooperativa: cooperativaStored })
   })
-  .catch(err => res.status(500).send({ message: `Error al guardar en la base de datos: ${err}` }))
+  .catch(err => res.status(500).send({ message: `Error al guardar la cooperativa en la base de datos: ${err}` }))
 }
 
 function updateCooperativa (req, res) {
   let cooperativaId = req.params.cooperativaId
   let cooperativa = req.body
-  Cooperativa.update(cooperativa, {
-    where: {
-      codigo: cooperativaId
-    }
+  Cooperativa.update(cooperativa, { where: { codigo: cooperativaId }, returning: true })
+  .then((cooperativaUpdate) => {
+    if(cooperativaUpdate[0] <= 0) return res.status(404).send({ message: `La cooperativa ${cooperativaId} no existe` })
+    res.status(200).send({ cooperativa: cooperativaUpdate[1] })
   })
-  .then(cooperativaUpdate => {
-    console.log(`1: ${cooperativaUpdate}`);
-  })
-  .then(cooperativaUpdate => {
-    console.log(`2: ${cooperativaUpdate}`);
-  })
-  .catch(err => res.status(500).send({ message: `Error al realizar la peticion: ${err}` }))
+  .catch(err => res.status(500).send({ message: `Error al actualizar la cooperativa en la base de datos: ${err}` }))
 }
 
 function deleteCooperativa (req, res) {
   let cooperativaId = req.params.cooperativaId
-  Cooperativa.destroy({
-    where: {
-      codigo: cooperativaId
-    }
+  Cooperativa.destroy({ where: { codigo: cooperativaId } })
+  .then(cooperativaCountDelete => {
+    if(cooperativaCountDelete <= 0) return res.status(404).send({ message: `La cooperativa ${cooperativaId} no existe` })
+    res.status(200).send({ message: `La cooperativa ${cooperativaId} ha sido eliminada` })
   })
-  .then(cooperativaDelete => {
-    console.log(`1: ${cooperativaUpdate}`);
-  })
-  .then(cooperativaDelete => {
-    console.log(`2: ${cooperativaUpdate}`);
-  })
-  .catch(err => res.status(500).send({ message: `Error al realizar la peticion: ${err}` }))
+  .catch(err => res.status(500).send({ message: `Error al eliminar la cooperativa en la base de datos: ${err}` }))
 }
 
 module.exports = {

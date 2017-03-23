0f8172c9fa23955f19b616cebc27747f7efae6db
vaec88 <victoreras88@hotmail.com>
2017-03-23 09:10:10 -0500

    Validate Cooperativas

--- models/cooperativa.js
+++ models/cooperativa.js
@@ -3,14 +3,49 @@
 const {Sequelize, sequelize} = require('./sequelizeConf')
 
 const CooperativaSchema = {
-  codigo: { type: Sequelize.STRING, primaryKey: true, allowNull: false },
-  ruc: { type: Sequelize.STRING, allowNull: false },
-  nombre: { type: Sequelize.STRING, allowNull: false },
-  gerente: { type: Sequelize.STRING, allowNull: false },
-  telefono: Sequelize.STRING,
+  codigo: {
+    type: Sequelize.STRING,
+    primaryKey: true,
+    allowNull: false,
+    validate: { notEmpty: { args: true, msg: 'Ingrese un codigo para la Cooperativa' } },
+    set: function(valCodigo) { return this.setDataValue('codigo', valCodigo.toUpperCase()) }
+  },
+  ruc: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      isNumeric: { args: true, msg: 'Ingrese solo numeros' },
+      notEmpty: { args: true, msg: 'Ingrese un numero de RUC' },
+      len: { args: 13, msg: 'El RUC debe tener 13 digitos' }
+    }
+  },
+  nombre: {
+    type: Sequelize.STRING, allowNull: false,
+    validate: {
+      notEmpty: { args: true, msg: 'Ingrese un nombre para la Cooperativa' },
+      is: { args: ["^[a-z]+$",'i'], msg: 'Ingrese solo letras' }
+    },
+    set: function(valNombre) { return this.setDataValue('nombre', valNombre.toUpperCase()) }
+  },
+  gerente: {
+    type: Sequelize.STRING, allowNull: false,
+    validate: {
+      notEmpty: { args: true, msg: 'Ingrese un nombre de Gerente' },
+      is: { args: ["^[a-z]+$",'i'], msg: 'Ingrese solo letras' }
+    },
+    set: function(valGerente) { return this.setDataValue('gerente', valGerente.toUpperCase()) }
+  },
+  telefono: {
+    type: Sequelize.STRING, allowNull: true, defaultValue: null,
+    validate: {
+      isNumeric: { args: true, msg: 'Ingrese solo numeros' },
+      notEmpty: { args: true, msg: 'Ingrese un numero de telefono' }
+    }
+  },
   correo: Sequelize.STRING,
   matriz: Sequelize.STRING
 }
+
 
 module.exports = sequelize.define(
   'cooperativa',

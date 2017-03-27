32ec35cc226c453d8252dfd3316238416172bfab
Adrian Conza <hola@adrianconza.com>
2017-03-27 11:53:09 -0500

    changes origen - destino

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,28 @@ const auth = require('../middlewares/auth')
 const express = require('express')
 const api = express.Router()
 
-const cooperativaCtrl = require('../controllers/cooperativa')
 
+const authCtrl = require('../controllers/auth')
+api.post('/signUp', authCtrl.signUp)
+api.post('/signIn', authCtrl.signIn)
+
+
+api.get('/ciudadOrigen', auth, (req, res) => {
+  res.status(200).send({
+    origen: ['Machala', 'Guayaquil', 'Cuenca']
+  })
+})
+api.get('/ciudadDestino/:origenId', auth, (req, res) => {
+  const origenId = req.params.origenId
+  console.log(origenId)
+  const ciudadades = ['Machala', 'Guayaquil', 'Cuenca']
+  ciudadades.splice(ciudadades.indexOf(origenId), 1)
+  res.status(200).send({
+    origen: ciudadades
+  })
+})
+
+const cooperativaCtrl = require('../controllers/cooperativa')
 api.get('/cooperativa', auth, cooperativaCtrl.getCooperativas)
 api.get('/cooperativa/:cooperativaId',  auth, cooperativaCtrl.getCooperativa)
 api.post('/cooperativa', auth, cooperativaCtrl.saveCooperativa)
@@ -14,15 +34,8 @@ api.delete('/cooperativa/:cooperativaId', auth, cooperativaCtrl.deleteCooperativ
 
 
 const personaCtrl = require('../controllers/persona')
-
 api.get('/persona', auth, personaCtrl.getPersonas)
 api.get('/persona/:personaId', auth, personaCtrl.getPersona)
 api.post('/persona', auth, personaCtrl.savePersona)
-
-const authCtrl = require('../controllers/auth')
-
-api.post('/signUp', authCtrl.signUp)
-api.post('/signIn', authCtrl.signIn)
-
 
 module.exports = api

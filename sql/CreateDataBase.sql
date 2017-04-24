7c0b09db462c44fd695710e7e4e502ed35cee7cb
Adrian Conza <hola@adrianconza.com>
2017-04-23 23:38:15 -0500

    changes sql

--- sql/CreateDataBase.sql
+++ sql/CreateDataBase.sql
@@ -90,7 +90,7 @@ CREATE SCHEMA oroticket AUTHORIZATION orocodigo;
 CREATE DOMAIN oroticket.asiento 									AS integer;
 CREATE DOMAIN oroticket.asiento_arr 							AS integer[];
 CREATE DOMAIN oroticket.cedula_ruc 								AS character varying(13);
-CREATE DOMAIN oroticket.codigo 										AS character varying(5);
+CREATE DOMAIN oroticket.codigo 										AS character varying(10);
 CREATE DOMAIN oroticket.codigo_seguridad_tarjeta 	AS character varying(4);
 CREATE DOMAIN oroticket.dinero 										AS numeric(9,2);
 CREATE DOMAIN oroticket.direccion 								AS character varying(300);
